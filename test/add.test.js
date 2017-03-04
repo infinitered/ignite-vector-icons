ee3216fa333344199b49012446a2fe19fdb40112
Jamon Holmgren <jamon@infinite.red>
2017-03-04 14:58:43 -0800

    Fixes test failures due to deprecation fixes

--- test/add.test.js
+++ test/add.test.js
@@ -1,22 +1,22 @@
 const test = require('ava')
 const sinon = require('sinon')
-const plugin = require('../index')
+const plugin = require('../plugin')
 
 test('adds itself', async t => {
   const addModule = sinon.spy()
-  const addComponentExample = sinon.spy()
+  const addPluginComponentExample = sinon.spy()
 
   const context = {
     ignite: {
       addModule: addModule,
-      addComponentExample: addComponentExample
+      addPluginComponentExample: addPluginComponentExample
     }
   }
 
   await plugin.add(context)
   t.true(addModule.calledWith('react-native-vector-icons', { link: true }))
   t.true(
-    addComponentExample.calledWith('vectorExample.js', {
+    addPluginComponentExample.calledWith('vectorExample.js', {
       title: 'Vector Icons'
     })
   )
